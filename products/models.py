cedcb7fd04ebe7060114cc3d3dc81a13c6714675
Alex Nicolescu <nicolescualexandru12@gmail.com>
2020-07-20 16:17:56 +0300

    Last commit

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.TextField()
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField('images/')
+    icon = models.ImageField('images/')
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def summary(self):
+        return self.body[:100]
+    
+    def pub_date_pretty(self):
+       return self.pub_date.strftime('%b %e %Y') 
+
+    def __str__(self):
+        return self.title
